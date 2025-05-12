3433d4a02f07b6f9abf5e17ae3071cad9e04e16e
Vickouma77 <Vickprestone20@gmail.com>
2025-05-12 15:01:22 +0300

    Implement main function to initialize logging and start GraphQL server

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use log::info;
+use rust_actix_juniper_graphql::server::server;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    server::init_logging();
+    info!("Starting GraphQL server");
+    server::start_server().await
+}
